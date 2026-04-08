8225a8ce8aed67b0e4338bd68880ae9e2d63bf9d
MichailImchikov <Coklexminer@gmail.com>
2026-04-08 20:11:08 +0300

    no message

--- Assets/Scripts/DoorActivateZoneMB.cs
+++ Assets/Scripts/DoorActivateZoneMB.cs
@@ -2,14 +2,22 @@ using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 
+public enum DoorOpenCondition
+{
+    None,
+    RequiresExitKey
+}
+
 public class DoorActivateZoneMB : NetworkBehaviour
 {
     public InteractZone interactZone;
-    
+
     [SyncVar]
     private bool isOpened = false;
-    
+
     public string HintText = "Open";
+
+    public DoorOpenCondition openCondition = DoorOpenCondition.None;
     
     public void Start()
     {
@@ -28,7 +36,33 @@ public class DoorActivateZoneMB : NetworkBehaviour
     private void OnInteract()
     {
         if (isOpened) return;
+
+        if (!CheckOpenCondition()) return;
+
         CmdOpenDoor();
+    }
+
+    private bool CheckOpenCondition()
+    {
+        switch (openCondition)
+        {
+            case DoorOpenCondition.RequiresExitKey:
+                var inventory = PlayerInventory.LocalInstance;
+                if (inventory == null) return false;
+
+                for (int i = 0; i < PlayerInventory.SlotCount; i++)
+                {
+                    if (inventory.GetSlot(i) is ExitKeyItem)
+                        return true;
+                }
+
+                Debug.Log("[DoorActivateZoneMB] Requires Exit Key to open");
+                return false;
+
+            case DoorOpenCondition.None:
+            default:
+                return true;
+        }
     }
     
     [Command(requiresAuthority = false)]
